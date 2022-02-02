0e0f62b99e942e43922e0c87d3d195f172dcf6b5
wesleyclzns <calazanscafe@outlook.com>
2022-02-02 14:59:11 -0300

    adição de links para futura extração

--- refs-origem.py
+++ refs-origem.py
@@ -6,6 +6,16 @@ asteroidesTroianoNetuno = 'https://pt.wikipedia.org/wiki/Troiano_de_Netuno'
 
 asteroidesApolo = 'https://pt.wikipedia.org/wiki/Asteroide_Apolo'
 
+asteroides = 'https://pt.wikipedia.org/wiki/Asteroide'
+
+craterasTerrestres = 'https://pt.wikipedia.org/wiki/Lista_de_crateras_de_impacto_na_Terra'
+
+listaAsteroidesNotaveis = 'https://pt.wikipedia.org/wiki/Lista_de_asteroides_not%C3%A1veis'
+
+listaDeAsteroides = 'https://pt.wikipedia.org/wiki/Lista_de_asteroides'
+
+objetosSistemaSolarTamanho = 'https://pt.wikipedia.org/wiki/Lista_de_objetos_do_Sistema_Solar_por_tamanho'
+
 satelitesSistemaSolar = 'https://pt.wikipedia.org/wiki/Lista_de_sat%C3%A9lites_naturais_do_Sistema_Solar'
 
 cinturaoAsteroides = 'https://pt.wikipedia.org/wiki/Cintura_de_asteroides'
